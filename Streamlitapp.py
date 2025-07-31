19efcd8077ea0529b0a5bce8231f4432ba971212
Sreekanthgandhodi <gandhodisree@gmail.com>
2025-07-31 21:31:47 +0530

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -15,30 +15,21 @@ name_on_order = st.text_input("Name on smoothie:")
 st.write("Name on your smoothie will be:", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'),col('Search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 ingredients_list= st.multiselect('choose upto 5 incrediants:',my_dataframe,max_selections=5
 )
 if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-      
-if smoothiefroot_response.status_code == 200:
-    try:
-        data = smoothiefroot_response.json()
-        st.dataframe(data, use_container_width=True)
-    except ValueError:
-        st.error(f"Invalid JSON received for {fruit_chosen}.")
-        st.text("Raw response:\n" + smoothiefroot_response.text)
-else:
-    st.error(f"Failed to fetch data for {fruit_chosen}. Status code: {smoothiefroot_response.status_code}")
-    st.text("Raw response:\n" + smoothiefroot_response.text)
-
-
+        sf_df =st.dataframe(data=my_dataframe, use_container_width=True) 
+        3st.dataframe(data=smoothiefroot_response.json())
 #st.write(ingredients_string)
 time_to_insert = st.button("Submit Order")
 if time_to_insert:
